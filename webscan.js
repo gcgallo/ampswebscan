171a33b09bad04677a56e26fc3e9b1da87d02a70
gcgallo <ggallo102@gmail.com>
2016-11-27 00:20:14 -0800

    Update webscan.js

--- webscan.js
+++ webscan.js
@@ -1,4 +1,5 @@
 var capture;
+var data = new FormData;
 
 function setup() {
   createCanvas(600, 480);
@@ -14,9 +15,28 @@ function draw() {
 }
 
 function mousePressed() {
-  saveFrames("out", "png", 1, 1, function (data){
-    print(data);
-    httpPost("https://api.ocr.space/parse/image", data, "json", function (ocrParsedResult));
+
+  saveFrames("out", "png", 1, 1, function(formData){
+	print(formData)
+	data.append("file", formData);
+	data.append("url", "https://gcgallo.github.io/ampswebscan/index.html");
+	data.append("language", "eng");
+	data.append("api", "c99af1a26988957");
+	data.append("isOverlayRequired", true);
+	
+	jQuery.ajax({
+
+		url: "https://api.ocr.space/parse/image",
+		data: formData,
+		dataType: 'form/multipart',
+		cache: false,
+		contentType: false,
+		processData: false,
+		type: 'json',
+	success: function (ocrParsedResult) {}})
+	
+    /*httpPost("https://api.ocr.space/parse/image", formData, "form/multipart", function(ocrParsedResult){
+		print(ocrParsedResult);});*/
   });
   
   //saveCanvas('out', 'png');
